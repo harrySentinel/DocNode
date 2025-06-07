573c566e8a13988d30da19c2b7cc949350289418
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-06-07 12:38:10 +0530

    app layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,24 +16,25 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className}`}
       >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-           {/* header */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* header */}
+          <Header />
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        {/* footer */}
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>Made with 💗 by Aditya</p>
-          </div>
-        </footer>
-          </ThemeProvider>
-      
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with 💗 by Aditya</p>
+            </div>
+          </footer>
+        </ThemeProvider>
+
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-     <div>
+     <div className="py-30">
       Hello there i am Aditya Srivastava !!
      <br />
      <Button>Click me</Button>
