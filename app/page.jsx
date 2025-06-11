4ee049ea363637eaa2580a044198c35545d37659
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-06-11 22:26:51 +0530

    added monthly subscription functionality using clerk

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Pricing from "@/components/pricing";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -99,6 +100,7 @@ export default function Home() {
 
           <div>
             {/* Pricing Table */}
+            <Pricing />
 
             <Card className="mt-12 bg-muted/20 border-emerald-900/30">
               <CardHeader>
